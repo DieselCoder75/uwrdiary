fb5da72780af5d1b251a8f284e1f6d210f9d7a3f
Janne Lind <janne.lind@salesforce.com>
2026-04-20 10:25:30 +0300

    app: function openModal(entryId = null, data = null) {

--- app.js
+++ app.js
@@ -721,6 +721,7 @@ function openModal(entryId = null, data = null) {
   // Other fields
   el('entry-duration').value  = data?.duration || 60;
   el('entry-comment').value   = data?.comment  || '';
+  updateCommentCounter();
   el('entry-distance').value  = data?.distance != null ? String(data.distance).replace('.', ',') : '';
   el('entry-heartrate').value = data?.avgHr    != null ? data.avgHr    : '';
   el('entry-maxhr').value     = data?.maxHr    != null ? data.maxHr    : '';
