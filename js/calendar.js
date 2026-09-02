68e1ab79a66ae5cc44fd64b41dd9371cfdc0800f
DieselCoder75 <jannelind@gmail.com>
2026-09-02 08:09:33 +0300

    deploy: deploy-v226

--- js/calendar.js
+++ js/calendar.js
@@ -48,6 +48,7 @@ const TEAM_EVENTS = [
   { start: '2026-10-03', end: '2026-10-04', label: 'Leiri – Kokkola',     dates: '3.–4.10.2026', link: 'https://www.uppopallo.fi/uutiset/naisten-avoin-maajoukkueleiri-kokko/' },
   { start: '2026-10-24', end: '2026-10-25', label: 'Varainkeruu/Leiri – Kouvola', dates: '24.–25.10.2026' },
   { start: '2026-12-12', end: '2026-12-13', label: 'Kierros/Leiri – Kokkola', dates: '12.–13.12.2026' },
+  { start: '2027-01-16', end: '2027-01-17', label: 'Kierros/Leiri – Hämeenlinna', dates: '16.–17.1.2027' },
   { start: '2027-01-23', end: '2027-01-24', label: 'SWE-FIN Camp III – Turku', dates: '23.–24.1.2027' },
   { start: '2027-04-24', end: '2027-04-25', label: 'Leiri – Turku',           dates: '24.–25.4.2027' },
   { start: '2027-05-15', end: '2027-05-22', label: 'MM-Kisat – Torremolinos', dates: '15.–22.5.2027', highlight: true },
